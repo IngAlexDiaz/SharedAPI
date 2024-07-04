9524ed43f67d7f49d0e615dc686c645f219a9ad9
IngAlexDiaz <ingenierodiaz02@gmail.com>
2024-07-04 15:57:59 -0400

    Add user Entity

---
+++ SharedAPI/Shared.Commond/Entities/User.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Shared.Common.Entities
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string FirtsName { get; set; } = null!;
+
+        [Required]
+        [MaxLength(100)]
+        public string LastName { get; set; } = null!;
+
+        [Required]
+        [MaxLength(20)]
+        [MinLength(20)]
+        public string Phone { get; set; } = null!;
+
+        [Required]
+        [EmailAddress]
+        [MaxLength(100)]
+        public string Email { get; set; } = null!;
+
+        [Required]
+        [MaxLength(20)]
+        [MinLength(6)]
+        public string Password { get; set; } = null!;
+    }
+}
